b480ad7ff17cb40959acac74e9c867d42cfe931a
raissafasya <raisyafasyasabila@upi.edu>
2026-06-07 17:20:46 +0700

    nambahin logo qris sm tunai

--- resources/views/tenant/stok/_form-restock.blade.php
+++ resources/views/tenant/stok/_form-restock.blade.php
@@ -51,19 +51,22 @@
 <div class="flex gap-2 mb-4">
     <button type="button" id="{{ $prefix }}RsBtnQRIS"
             class="restock-pay-btn" onclick="setRestockMetode('{{ $prefix }}', 'qris')">
-        <svg class="w-5 h-5 mx-auto mb-0.5" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5z"/>
+        <svg class="w-5 h-5 mx-auto mb-1" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1 -1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 14.25h3v3h-3v-3ZM16.5 17.25h3v3h-3v-3ZM19.5 14.25h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM16.5 14.25h.75v.75h-.75v-.75ZM19.5 18h.75v.75h-.75v-.75ZM18 15.75h.75v.75h-.75v-.75ZM15.75 18h.75v.75h-.75v-.75Z" />
         </svg>
         QRIS
     </button>
+    
     <button type="button" id="{{ $prefix }}RsBtnTUNAI"
             class="restock-pay-btn active" onclick="setRestockMetode('{{ $prefix }}', 'tunai')">
-        <svg class="w-5 h-5 mx-auto mb-0.5" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75"/>
+        <svg class="w-5 h-5 mx-auto mb-1" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5h16.5a1.5 1.5 0 0 1 1.5 1.5v11.25a1.5 1.5 0 0 1-1.5 1.5H3.75a1.5 1.5 0 0 1-1.5-1.5V6.5a1.5 1.5 0 0 1 1.5-1.5Zm6.45 7.5a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0Z" />
         </svg>
         TUNAI
     </button>
 </div>
+
 <input type="hidden" name="metode_bayar" id="{{ $prefix }}RsMetode" value="tunai">
 
 {{-- Ringkasan pembayaran supplier --}}
